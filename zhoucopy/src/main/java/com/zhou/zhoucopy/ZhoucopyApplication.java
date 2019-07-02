3832626ddc94c954e8dce945f1806cfcc709ff67
supermaniszhou <741963634@qq.com>
2019-07-02 21:57:59 +0800

    add

---
+++ zhoucopy/src/main/java/com/zhou/zhoucopy/ZhoucopyApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.zhoucopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhoucopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhoucopyApplication.class, args);
+    }
+
+}
---
+++ zhoucopy/src/test/java/com/zhou/zhoucopy/ZhoucopyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhou.zhoucopy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhoucopyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
