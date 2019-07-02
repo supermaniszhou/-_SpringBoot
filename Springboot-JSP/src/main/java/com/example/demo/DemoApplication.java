43e3c40d75372325a1dbdd350ebb87b2aaa3f404
supermaniszhou <741963634@qq.com>
2019-07-02 15:50:13 +0800

    xiugai

--- Springboot-JSP/src/main/java/com/example/demo/DemoApplication.java
+++ Springboot-JSP/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.demo.mapper")
+@MapperScan(basePackages = {"com.example.demo.mapper.*"})
 public class DemoApplication {
 
     public static void main(String[] args) {
