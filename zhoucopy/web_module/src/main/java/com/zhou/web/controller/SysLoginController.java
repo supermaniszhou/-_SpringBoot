220b555cbed01092554f2d7c05ca15177abc19f6
supermaniszhou <741963634@qq.com>
2019-07-03 16:50:38 +0800

    登录控制层

---
+++ zhoucopy/web_module/src/main/java/com/zhou/web/controller/SysLoginController.java
@@ -0,0 +1,12 @@
+package com.zhou.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 周刘成   2019/7/3
+ */
+@RestController
+public class SysLoginController {
+
+
+}
