4278c1f356d4fd1196a45e0d7c994a00af797261
supermaniszhou <741963634@qq.com>
2019-07-08 10:55:56 +0800

    修改

--- FamilyBoot/src/main/java/com/family/zhou/ZhouApplication.java
+++ FamilyBoot/src/main/java/com/family/zhou/ZhouApplication.java
@@ -1,10 +1,12 @@
 package com.family.zhou;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.family.zhou.mapper")
 public class ZhouApplication {
 
     public static void main(String[] args) {
