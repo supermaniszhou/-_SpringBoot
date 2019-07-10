05cb524c979cb276cf55e63b306940521971c93c
supermaniszhou <741963634@qq.com>
2019-07-10 08:58:32 +0800

    修改

--- FamilyBoot/src/main/java/com/family/zhou/controller/sys/IndexController.java
+++ FamilyBoot/src/main/java/com/family/zhou/controller/sys/IndexController.java
@@ -1,9 +1,12 @@
 package com.family.zhou.controller.sys;
 
 import com.family.zhou.controller.common.BaseController;
+import com.family.zhou.service.SysUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class IndexController extends BaseController {
 
-    @RequestMapping("/index")
+    @Resource
+    private SysUserService userService;
+
+    @GetMapping("/index")
     public String index(HttpServletRequest request, HttpServletResponse response) {
         return "index";
     }
