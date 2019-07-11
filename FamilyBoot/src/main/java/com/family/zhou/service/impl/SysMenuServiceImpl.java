2a59d70c8d03d763559639a92a25fc140d59bd27
supermaniszhou <741963634@qq.com>
2019-07-11 09:55:31 +0800

    update

--- FamilyBoot/src/main/java/com/family/zhou/service/impl/SysMenuServiceImpl.java
+++ FamilyBoot/src/main/java/com/family/zhou/service/impl/SysMenuServiceImpl.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,6 +26,32 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
 
     @Override
     public List<SysMenu> selectAllMenu() {
-        return sysMenuMapper.selectAllMenu();
+        List<SysMenu> list = sysMenuMapper.selectAllMenu();
+
+        List<SysMenu> parentList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            SysMenu menu = list.get(i);
+            if (menu.getParentId() == 0) {
+                menu.setChildren(getChildren(list, menu.getMenuId()));
+                parentList.add(menu);
+            }
+        }
+        return parentList;
     }
+
+    public List<SysMenu> getChildren(List<SysMenu> list, long parentId) {
+        List<SysMenu> childList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            SysMenu sysMenu = list.get(i);
+            if (sysMenu.getParentId() == parentId) {
+                childList.add(sysMenu);
+            }
+        }
+        for (int j = 0; j < childList.size(); j++) {
+            SysMenu menu = childList.get(j);
+            menu.setChildren(getChildren(list, menu.getMenuId()));
+        }
+        return childList;
+    }
+
 }
